bd936d12e0f98fb7e7f453b7690150a5a3c94aa8
wooricha <ckdnfl4769@naver.com>
2019-08-26 22:43:41 +0900

    조건문

--- PHP 생활코딩/조건문/conditional_statements.php
+++ PHP 생활코딩/조건문/conditional_statements.php
@@ -6,8 +6,6 @@
 </head>
 <body>
 	<?php
-	//conditional statements(조건문)
-	//if (표현식) {실행될 코드}
 		if(true){
 			echo '2<br>';
 		}
@@ -17,9 +15,6 @@
 		} else{
 			echo '2-2<br>';
 		}
-		//check value exists(값이 있는지 체크)
-		//isset(값의 유무에 따라 boolean을 도출하는 함수)
-		//unset(값을 지워주는 함수)
 		$a = "test";
     $b = "anothertest";
 
@@ -30,6 +25,8 @@
 
 		var_dump(isset($a)); //false
 		var_dump(isset($a, $b)); //false
+		//isset(값의 유무에 따라 boolean을 도출하는 함수)
+		//unset(값을 지워주는 함수)
 	?>
 </body>
 </html>

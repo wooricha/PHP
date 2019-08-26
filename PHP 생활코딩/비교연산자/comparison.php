d4a3d07fa031e3128bdc38a41eb30e94be2551e5
wooricha <ckdnfl4769@naver.com>
2019-08-26 22:43:22 +0900

    비교연산자

--- PHP 생활코딩/비교연산자/comparison.php
+++ PHP 생활코딩/비교연산자/comparison.php
@@ -6,12 +6,10 @@
 </head>
 <body>
 	<?php
-		//comparison operators(비교연산자) & boolean data type
 		var_dump('11'); //-> string(11)
 		var_dump(1==1); //-> bool(true)
 		var_dump(1==2); //-> bool(false)
-		//비교연산자는 boolean이라는 데이터타입을 만들어낸다
-		//boolean은 참(true), 거짓(false)로 나뉜다
+		//var_dump()는 데이터 타입을 나타내주는 함수
 	?>
 </body>
 </html>

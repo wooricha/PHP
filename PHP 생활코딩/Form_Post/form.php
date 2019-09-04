35e1bb358c7d442b840ebaac6aeb993cb0d02067
wooricha <ckdnfl4769@naver.com>
2019-09-04 22:33:41 +0900

    form&post

---
+++ PHP 생활코딩/Form_Post/form.php
@@ -0,0 +1,6 @@
+<?php
+  echo "<p>title : ".$_GET['title']."</p>";
+  //title을 가져옴(name 값)
+  echo "<p>description : ".$_GET['description']."</p>";
+  //description을 가져옴(name 값)
+ ?>
---
+++ PHP 생활코딩/Form_Post/form2.php
@@ -0,0 +1,4 @@
+<?php
+  file_put_contents('../'.$_POST['title'], $_POST['description'])
+// PHP3 파일 생성
+ ?>
